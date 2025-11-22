8d7f46819ffff63b12211effadf4ef4dce19c99d
NaveenK <kusakulanaveen@gmail.com>
2025-11-23 00:48:51 +0530

    feat: Introduce `GetApiService` for fetching positions and options data, integrating it into the client and decision maker.

--- get_api_service.py
+++ get_api_service.py
@@ -28,3 +28,25 @@ class PositionsService:
         except Exception as e:
             print(f"Exception while fetching positions: {e}")
             return None
+
+
+
+    def get_options(self) -> Optional[Dict]:
+        payload = (
+            f"jData={{\n\t\"uid\": \"{self.config.CLIENT_ID}\",\n\t\"actid\": \"{self.config.CLIENT_ID}\"\n}}"
+            f"&jKey={self.auth_token}"
+        )
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": "insomnia/11.0.1",
+        }
+
+        try:
+            response = requests.request("POST", self.config.OPTIONS_URL, data=payload, headers=headers)
+            if response.status_code == 200:
+                return response.json()
+            print(f"Error fetching positions: {response.status_code}, {response.text}")
+            return None
+        except Exception as e:
+            print(f"Exception while fetching positions: {e}")
+            return None
